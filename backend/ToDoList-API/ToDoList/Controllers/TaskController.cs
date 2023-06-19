a20deea34aeffd7f94178ba6807a8599097b817a
kek145 <yurabogomolov2004@gmail.com>
2023-06-19 18:25:23 +0300

    UPDATE | Add crud operations in TaskController

--- backend/ToDoList-API/ToDoList/Controllers/TaskController.cs
+++ backend/ToDoList-API/ToDoList/Controllers/TaskController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace ToDoList.Controllers
 {
@@ -14,6 +16,30 @@ namespace ToDoList.Controllers
         public TaskController(ILogger<TaskController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpPost("CreateTask")]
+        public async Task<IActionResult> CreateTaskAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetAllTask")]
+        public async Task<IActionResult> GetAllTaskAsync()
+        {
+            return Ok();
+        }
+
+        [HttpPut("UpdateTask/{taskid}")]
+        public async Task<IActionResult> UpdateTaskAsync()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("DeleteTask/{taskid}")]
+        public async Task<IActionResult> DeleteTaskAsync()
+        {
+            return Ok();
         }
     }
 }
