b7b879f559c268b0ad01f61d53f73ec5da6ff04f
kek145 <yurabogomolov2004@gmail.com>
2023-06-20 21:47:28 +0300

    create components

--- frontend/ToDoList-Client/src/app/app.module.ts
+++ frontend/ToDoList-Client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
