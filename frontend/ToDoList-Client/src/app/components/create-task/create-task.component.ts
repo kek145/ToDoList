1294917c9db6cca3f8a56cf9e9754b9b6c419639
kek145 <yurabogomolov2004@gmail.com>
2023-06-21 12:46:07 +0300

    add binding fields to the model

--- frontend/ToDoList-Client/src/app/components/create-task/create-task.component.ts
+++ frontend/ToDoList-Client/src/app/components/create-task/create-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Priority } from 'src/app/enums/priority.enum';
+import { ITaskModel } from 'src/app/models/task.model';
 
 @Component({
   selector: 'app-create-task',
@@ -7,9 +9,22 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./create-task.component.css']
 })
 export class CreateTaskComponent implements OnInit {
-  constructor(private titleService: Title) {}
+  task: ITaskModel;
+  constructor(private titleService: Title) { 
+    this.task = {
+      title: '',
+      description: '',
+      status: false,
+      priority: Priority.easy,
+      created: new Date()
+    };
+  }
 
   ngOnInit(): void {
     this.titleService.setTitle('Create Task');
+  }
+
+  submitForm() {
+    console.log(`title: ${this.task.title}, description: ${this.task.description}, status: ${this.task.status}, priority: ${this.task.priority}, created: ${this.task.created}`)
   }
 }
