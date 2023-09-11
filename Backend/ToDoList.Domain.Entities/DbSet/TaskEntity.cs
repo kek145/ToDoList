fe16d579e9bba930410b445334f3bb8571ca7f95
kek145 <yurabogomolov2004@gmail.com>
2023-09-11 22:13:35 +0300

    UPDATE | Added task entity and priority enum

---
+++ Backend/ToDoList.Domain.Entities/DbSet/TaskEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ToDoList.Domain.Entities.Enums;
+
+namespace ToDoList.Domain.Entities.DbSet;
+
+public class TaskEntity
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public bool Status { get; set; }
+    public Priority Priority { get; set; }
+    public DateTime Deadline { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
