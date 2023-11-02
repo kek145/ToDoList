71fbfae783a83dcefb37b02e060f3fb7b40ad992
kek145 <yurabogomolov2004@gmail.com>
2023-11-02 20:12:02 +0200

    UPDATE | Added db context

--- ToDoList.Api/Program.cs
+++ ToDoList.Api/Program.cs
@@ -1,12 +1,25 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoList.DAL.Configurations.DataContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(Program).Assembly));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")).UseSnakeCaseNamingConvention();
+});
 
 var app = builder.Build();
 
---
+++ ToDoList.DAL.Configurations/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Entities.DbSet;
+using ToDoList.DAL.Configurations.Configurations;
+
+namespace ToDoList.DAL.Configurations.DataContext;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<TaskEntity> Tasks { get; set; } = null!;
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TaskEntityConfiguration());
+    }
+}
