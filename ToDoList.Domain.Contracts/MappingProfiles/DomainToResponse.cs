173e28d0e8641dced6f9e7fdeb0a386544862713
kek145 <yurabogomolov2004@gmail.com>
2023-11-02 20:45:33 +0200

    UPDATE | Added mappings

---
+++ ToDoList.Domain.Contracts/MappingProfiles/DomainToResponse.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ToDoList.Domain.Entities.DbSet;
+using ToDoList.Domain.Contracts.Request;
+using ToDoList.Domain.Contracts.Response;
+
+namespace ToDoList.Domain.Contracts.MappingProfiles;
+
+public class DomainToResponse : Profile
+{
+    public DomainToResponse()
+    {
+        CreateMap<TaskEntity, GetTaskResponseDto>()
+            .ForMember(dest => dest.Id,
+                opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Title,
+                opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description,
+                opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Status,
+                opt => opt.MapFrom(src => src.Status))
+            .ForMember(dest => dest.Priority,
+                opt => opt.MapFrom(src => src.Priority))
+            .ForMember(dest => dest.Deadline,
+                opt => opt.MapFrom(src => src.DeadLine));
+    }
+}
