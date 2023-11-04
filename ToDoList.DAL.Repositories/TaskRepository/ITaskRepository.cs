716e6cfc8154b6cdf9eb97dde4f135ca627e1021
kek145 <yurabogomolov2004@gmail.com>
2023-11-04 08:35:09 +0200

    UPDATE | Added UnitOfWork pattern and TaskRepository

---
+++ ToDoList.DAL.Repositories/TaskRepository/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using ToDoList.Domain.Entities.DbSet;
+using ToDoList.DAL.Repositories.GenericRepository;
+
+namespace ToDoList.DAL.Repositories.TaskRepository;
+
+public interface ITaskRepository : IGenericRepository<TaskEntity>
+{
+    
+}
---
+++ ToDoList.DAL.Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ToDoList.DAL.Repositories.TaskRepository;
+
+namespace ToDoList.DAL.Repositories.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    ITaskRepository TaskRepository { get; }
+    Task<bool> CommitAsync();
+}
