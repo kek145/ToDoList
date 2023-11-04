79b1e874be40ce610b45f76ae6a958a76b578151
kek145 <yurabogomolov2004@gmail.com>
2023-11-04 07:57:17 +0200

    UPDATE | Added entities, dtos and mappings by request and response

---
+++ ToDoList.Domain.Contracts/MappingProfiles/RequestToDomain.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ToDoList.Domain.Entities.DbSet;
+using ToDoList.Domain.Contracts.Request;
+
+namespace ToDoList.Domain.Contracts.MappingProfiles;
+
+public class RequestToDomain : Profile
+{
+    public RequestToDomain()
+    {
+        CreateMap<CreateTaskRequestDto, TaskEntity>()
+            .ForMember(dest => dest.Title,
+                opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description,
+                opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Priority,
+                opt => opt.MapFrom(src => src.Priority))
+            .ForMember(dest => dest.Deadline,
+                opt => opt.MapFrom(src => src.Deadline));
+    }
+}
---
+++ ToDoList.Domain.Entities/Enum/Priority.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.Domain.Entities.Enum;
+
+public enum Priority
+{
+    Easy = 1,
+    Medium = 2,
+    Hard = 3
+}
