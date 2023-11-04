eb8ee233d0287b620c12693e06b6d4a5d28f9910
kek145 <yurabogomolov2004@gmail.com>
2023-11-04 08:17:40 +0200

    UPDATE | Added Database context and configurations entities

--- ToDoList.Api/Program.cs
+++ ToDoList.Api/Program.cs
@@ -1,12 +1,24 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoList.DAL.Configurations.DataContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
+        .UseSnakeCaseNamingConvention();
+});
 
 var app = builder.Build();
 
