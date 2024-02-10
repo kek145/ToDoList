554387f017257ef719b263e919b2b04890124542
kek145 <yurabogomolov2004@gmail.com>
2024-02-10 12:08:20 +0200

    UPT

---
+++ ToDoList.Frontend/src/models/response/IBaseResponse.model.ts
@@ -0,0 +1,8 @@
+import { HttpStatusCode } from "@angular/common/http";
+
+export interface IBaseResponseModel<T> {
+    statusCode: HttpStatusCode;
+    message: string;
+    data: T
+    errors: string[]
+}
